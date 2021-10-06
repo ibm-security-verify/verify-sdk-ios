a4a5b4858aede7f1e0918fdc37c9bda80127296c
Craig Pearson <craigaps@au1.ibm.com>
2021-10-06 13:10:50 +1000

    Initial commit of SPM

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IBM Security Verify",
+    platforms: [
+            .macOS(.v11), .iOS(.v14)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "FIDO2",
+            targets: ["FIDO2"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "FIDO2",
+            path: "sdk/fido2/Sources",
+            exclude: ["Info.plist"]),
+        .testTarget(
+            name: "FIDO2 Tests",
+            dependencies: ["FIDO2"],
+            path: "sdk/fido2/Tests"),
+    ]
+)
